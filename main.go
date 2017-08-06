226ead6e0f43e01fcb656225a7351da555dbe06b
edfungus <edfungus@gmail.com>
2017-08-06 15:57:21 -0700

    Cleaned up

--- main.go
+++ main.go
@@ -11,14 +11,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Login!"))
-}
-
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
-	r.HandleFunc("/login", login)
 
 	log.Fatal(http.ListenAndServe(":80", r))
 }
